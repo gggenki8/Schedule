5bd1121415b54fc3b50b84906e844008d854f46c
廣城弦貴 <genki@hiroshijougentakanoMacBook-Air.local>
2025-05-12 18:00:18 +0900

    動作確認済み

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_05_08_060210) do
+ActiveRecord::Schema.define(version: 2025_05_12_085727) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
